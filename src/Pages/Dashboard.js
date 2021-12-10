df789b5c2a97aa68f0ce26bd63ec87df4f086ea9
Kyle McClary <kyle@microscape.xyz>
2021-12-10 09:32:12 -0800

    authentication and routing

---
+++ src/Pages/Dashboard.js
@@ -0,0 +1,42 @@
+import '../App.css';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import awsconfig from '../aws-exports'
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+//import { listTodos } from '../graphql/queries';
+//import { useState, useEffect } from 'react';
+
+Amplify.configure(awsconfig);
+
+function Dashboard() {
+
+    // const [data, setData] = useState([])
+
+    // useEffect(() => {
+    //     fetchdata()
+    // }, []);
+
+    // const fetchdata = async () => {
+    //     try {
+    //         const foundData = await API.graphql(graphqlOperation(listTodos))
+    //         const dataList = foundData.data.listTodos.items;
+    //         console.log('data list', dataList)
+    //     }
+    //     catch (error) {
+    //         console.log('error fetchting', error)
+    //     }
+    // }
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AmplifySignOut />
+        <h2>My App Content</h2>
+      </header>
+    </div>
+  );
+}
+
+export default withAuthenticator(Dashboard);
